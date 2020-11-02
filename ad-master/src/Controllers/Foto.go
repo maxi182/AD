d2548467cdcd63b5dfee477070b2f66c444a670f
Santiago <santiago@cerfoglio.com>
2020-11-02 14:59:29 -0300

    Fotos v3

--- ad-master/src/Controllers/Foto.go
+++ ad-master/src/Controllers/Foto.go
@@ -2,29 +2,51 @@ package Controllers
 
 import (
 	"first-api/Models"
-	"fmt"
 	"net/http" //https://golang.org/pkg/net/http/
 	"github.com/gin-gonic/gin"
  )
+
+type fotoStatus struct {
+    Filename string
+	UploadFailed bool
+	Error error
+}
 
 func UploadFotos(c *gin.Context) {
 	var fotos []Models.FotoUpload
 	c.BindJSON(&fotos)
 
- 	err := Models.UploadFotos(&fotos)
-	if err != nil {
-		fmt.Println(err.Error())
+	var statusFotos []fotoStatus
+	hayError := false
+
+	for i := 0; i < len(fotos); i++ {
+		foto := (fotos)[i]
+
+		err := Models.UploadFoto(&foto)
+
+		statusError := false
+
+		if err != nil{
+			statusError = true
+			hayError = true
+		}
+
+		status := fotoStatus{Filename: foto.Filename, Error: err, UploadFailed: statusError}
+		statusFotos = append(statusFotos, status)
+	}
+
+	if hayError {
 		c.JSON(http.StatusNotFound, gin.H{
+			"fotos" : statusFotos,
 			"error" : gin.H { 
 			"status":  400,
-			"message": err.Error(),
+			"message": "La subida de fotos contiene errores",
 		}})
 		c.AbortWithStatus(http.StatusNotFound)
 	} else {
 		c.JSON(http.StatusOK, gin.H{
+			"fotos" : statusFotos,
 			"status":  http.StatusOK,
 		})
-		
-		//fmt.Println("reclamo_creado", rec.ID)
 	}
 }
--- ad-master/src/Models/Foto.go
+++ ad-master/src/Models/Foto.go
@@ -1,48 +1,41 @@
 package Models
 
 import (
-	"first-api/Utils"
 	"bytes"
 	"first-api/Config"
-	"github.com/jlaffaye/ftp"
+	"first-api/Utils"
 	"time"
-	"log"
-	//"fmt"
+
+	"github.com/jlaffaye/ftp"
 )
 
 //GetAllFotosByComentario etch all propiedad data
 func GetAllFotosByComentario(foto *[]Foto, comentarioId string) (err error) {
 
 	if err = Config.DB.Model(&Foto{}).Select("*").Joins("inner join Comentarios on Comentarios.Id = Fotos.id").Where("Fotos.ComentarioId = ?", comentarioId).Find(&foto).Error; err != nil {
-			 return err
-		}
+		return err
+	}
 	return nil
 }
 
-func UploadFotos(fotos *[]FotoUpload) (err error) {
-	//c, err := ftp.Dial("190.55.95.131:3307", ftp.DialWithTimeout(5*time.Second))
+func UploadFoto(foto *FotoUpload) (err error) {
 	c, err := ftp.Dial("190.55.95.131:21", ftp.DialWithTimeout(5*time.Second))
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	
+
 	err = c.Login("reclamo@cerfoglio.com", "Uade12345")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
+	fotoBytes := Utils.DecodeBase64(foto.Data)
 
-	for i := 0; i < len(*fotos); i++ {
-		//timestamp := time.Now().Unix()
-		foto := (*fotos)[i]
-		fotoBytes := Utils.DecodeBase64(foto.Data)
+	data := bytes.NewBuffer(fotoBytes)
+	err = c.Stor(foto.Filename, data)
 
-		data := bytes.NewBuffer(fotoBytes)
-		//filename := fmt.Sprint(timestamp) + ".jpg"
-		err = c.Stor(foto.Filename, data)
-		if err != nil {
-			panic(err)
-		}
+	if err != nil {
+		return err
 	}
 
 	return nil
